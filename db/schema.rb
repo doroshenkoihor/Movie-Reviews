b6f5f8998f6523ad5d2e82d54b020d1215a5c599
doroshenkoihor <happiestwino@gmail.com>
2022-09-08 19:09:45 +0200

    Review scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_223742) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_08_165629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_223742) do
     t.integer "image_file_size"
     t.string "image_content_type"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
