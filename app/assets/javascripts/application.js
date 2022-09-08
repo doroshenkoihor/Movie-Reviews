34374f7b77eacc86a1c76c8645ededb254f8b35d
doroshenkoihor <happiestwino@gmail.com>
2022-09-08 23:27:19 +0200

    Convert review ratings to stars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,3 @@
 //= require jquery
+//= require jquery.raty
 //= require bootstrap-sprockets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_165629) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_08_171034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_165629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
