327bb33e74ac321f9edcf7e76c872cdf19fef62c
doroshenkoihor <happiestwino@gmail.com>
2022-09-09 01:58:34 +0200

    Convert review ratings to stars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_url
   end
+
+  def after_sign_out_path_for(resource)
+    root_url
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+
   has_many :movies
   has_many :reviews, dependent: :destroy
 end
